be2dfd5853e706d523014430846aa2f0c40d8640
Rodrigo Dorat <rdoratm@gmail.com>
2024-08-01 11:40:39 -0400

    :sparkles: Configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Product-MS');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -10,6 +12,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(envs.port);
+  logger.log(`Running on port: ${envs.port}`);
 }
 bootstrap();

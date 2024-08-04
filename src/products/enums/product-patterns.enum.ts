8b56886e9c0bf8151f47d9f1f5de05c9aa9199c0
Rodrigo Dorat <rdoratm@gmail.com>
2024-08-03 22:28:43 -0400

    :sparkles: use enums and index files

---
+++ src/products/enums/product-patterns.enum.ts
@@ -0,0 +1,7 @@
+export enum ProductPatterns {
+  createProduct = 'create_product',
+  findAllProducts = 'find_all_products',
+  findOneProduct = 'find_one_product',
+  updateProduct = 'update_product',
+  deleteProduct = 'delete_product',
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,32 +4,33 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto';
+import { ProductPatterns } from './enums';
 
-@Controller('products')
+@Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @MessagePattern({ cmd: 'create_product' })
+  @MessagePattern({ cmd: ProductPatterns.createProduct })
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @MessagePattern({ cmd: 'find_all_products' })
+  @MessagePattern({ cmd: ProductPatterns.findAllProducts })
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @MessagePattern({ cmd: 'find_one_product' })
+  @MessagePattern({ cmd: ProductPatterns.findOneProduct })
   findOne(@Payload('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
-  @MessagePattern({ cmd: 'update_product' })
+  @MessagePattern({ cmd: ProductPatterns.updateProduct })
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto);
   }
 
-  @MessagePattern({ cmd: 'delete_product' })
+  @MessagePattern({ cmd: ProductPatterns.deleteProduct })
   remove(@Payload('id') id: string) {
     return this.productsService.softDelete(+id);
   }

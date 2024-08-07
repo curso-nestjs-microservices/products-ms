05f85a3c6984f272125a89fd9d56d587c367f922
Rodrigo Dorat <rdoratm@gmail.com>
2024-08-07 14:42:30 -0400

    :bug: Fix hard coded configs

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     {
       transport: Transport.NATS,
       options: {
-        servers: ['nats://localhost:4222'],
+        servers: envs.natsServers,
       },
     },
   );
